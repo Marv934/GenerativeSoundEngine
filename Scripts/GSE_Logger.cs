de033d018976b449da4cde043a78a5074e0070d3
Marv934 <67321320+Marv934@users.noreply.github.com>
2021-12-09 17:04:36 +0100

    Update GSE_Logger.cs

--- Scripts/GSE_Logger.cs
+++ Scripts/GSE_Logger.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Globalization;
 using System;
 using UnityEngine;
 
